aaf2e3c7b8a1ea3af39527d2e26f5e564d50fffd
Lovepreet <pragra48@gmail.com>
2026-06-04 22:15:13 -0400

    Jenkin - 1

--- src/jun02/Exceptiondemo.java
+++ src/jun02/Exceptiondemo.java
@@ -1,45 +1,45 @@
-package jun02;
-
-import java.io.IOException;
-import java.sql.SQLException;
-
-public class Exceptiondemo {
-
-// primitive data types - int, char, double
-    // collections  - objects - Integer, Double, Char
-    public static void main(String[] args) {
-        Account account = new Account();
-        String input ="1000"; //"One thousand" convert string to int
-        try {
-            int amount = Integer.parseInt(input); // 1000
-            account.withdraw(amount);
-        } catch (NumberFormatException e){
-            System.out.println("Please enter a valid number");
-//        } catch (InsufficientException e) {
-            System.out.println("You dont have sufficient balance to withdraw");
-        } catch (NullPointerException e){
-            System.out.println("Null pointer ");
-        } catch (Exception e){
-            System.out.println("Something went wrong");
-        }
-
-        try {
-            // db operation
-        } catch (SQLException e){
-//            reconnectDB();
-        } catch (IOException e){
-            // read from backup file
-        }
-
-    }
-}
-
-// nullpointer
-// Insufficientbalance
-// Numberformatexception
-class Account{
-
-    void withdraw(int amount){
-
-    }
-}
+//package jun02;
+//
+//import java.io.IOException;
+//import java.sql.SQLException;
+//
+//public class Exceptiondemo {
+//
+//// primitive data types - int, char, double
+//    // collections  - objects - Integer, Double, Char
+//    public static void main(String[] args) {
+//        Account account = new Account();
+//        String input ="1000"; //"One thousand" convert string to int
+//        try {
+//            int amount = Integer.parseInt(input); // 1000
+//            account.withdraw(amount);
+//        } catch (NumberFormatException e){
+//            System.out.println("Please enter a valid number");
+////        } catch (InsufficientException e) {
+//            System.out.println("You dont have sufficient balance to withdraw");
+//        } catch (NullPointerException e){
+//            System.out.println("Null pointer ");
+//        } catch (Exception e){
+//            System.out.println("Something went wrong");
+//        }
+//
+//        try {
+//            // db operation
+//        } catch (SQLException e){
+////            reconnectDB();
+//        } catch (IOException e){
+//            // read from backup file
+//        }
+//
+//    }
+//}
+//
+//// nullpointer
+//// Insufficientbalance
+//// Numberformatexception
+//class Account{
+//
+//    void withdraw(int amount){
+//
+//    }
+//}
--- src/sideCourse/jun04/readMe.md
+++ src/sideCourse/jun04/readMe.md
@@ -24,10 +24,56 @@ PR flow
 Revert - Undo a commit by creating a new commit (history will be there )
 Reset - Go back in time and erase a commit (history will be changed)
 
+git revert <commit-hash>
+git reset --hard <commit-hash> or git reset --hard head~1
+
+use revert when code is pushed to remote branch
+use reset when changes are in your local only
 
 C1 -- C2 -- C3 -- C4 -- C5
 
-bsdjwgdjwebdjkwebk
 
 
-nkfewjhfewnfekwkf
+What happen once code is pushed to github ? deployment
+
+github ------------ server =-------------- pragra.com
+
+
+Student.java ----- javac Student.java ----- Student.class (bytecode)
+
+
+JAR (Java Archive)
+
+Maven -> It is used to convert java code to jar. BUILD AUTOMATION TOOL
+
+
+# CI/CD - Continuous integration/ Continuous delivery
+It is automated workflow which will help to build, test and deploy the code.
+
+
+- Jenkin
+- Github Action
+
+- CI ->  When you write the code & push to github/bitbucket/gitlab, it will get 
+integrated to github. each merge will get tested and build.
+
+- CD -> Code is ready to deploy to different environment.
+
+# Environment
+Dev
+QA
+UAT 
+Staging
+PROD 
+COB - continuity of business (exact replica of prod)
+
+
+IntelliJ -- github --- Build(Junit test) ----- code quality(SonarQube) ----
+QA (Integration testing) ------   UAT ---- CHG req   ------ Production 
+
+
+ test--- build (java classes to jar)---- docker image --- stored in registry (dockerhub)---- 
+ pull docker image ----- deploy the docker image 
+ 
+
+Jenkin
